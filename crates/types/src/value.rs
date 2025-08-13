ffe3b617ff838b402349c2a49757978052b40faa
allnil <nil.medvedev17@gmail.com>
2025-08-13 11:04:07 +0100

    feat: work on types and proto

--- crates/types/src/value.rs
+++ crates/types/src/value.rs
@@ -1,1 +1,8 @@
 pub struct Value;
+
+use serde::{Deserialize, Serialize};
+
+use crate::proto;
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Copy, Serialize, Deserialize)]
+pub struct ValueId(u64);
