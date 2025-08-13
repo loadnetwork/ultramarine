2573b2e47bb7282ba70a11391e7306331f779dea
allnil <nil.medvedev17@gmail.com>
2025-08-13 08:39:41 +0100

    feat: work on cli, update deps

---
+++ crates/types/src/height.rs
@@ -0,0 +1,73 @@
+use core::fmt;
+
+use malachitebft_proto::{Error as ProtoError, Protobuf};
+use serde::{Deserialize, Serialize};
+
+/// A blockchain height
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+pub struct Height(u64);
+
+impl Height {
+    pub const fn new(height: u64) -> Self {
+        Self(height)
+    }
+
+    pub const fn as_u64(&self) -> u64 {
+        self.0
+    }
+
+    pub fn increment(&self) -> Self {
+        Self(self.0 + 1)
+    }
+
+    pub fn decrement(&self) -> Option<Self> {
+        self.0.checked_sub(1).map(Self)
+    }
+}
+
+impl Default for Height {
+    fn default() -> Self {
+        Height(1)
+    }
+}
+
+impl fmt::Display for Height {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl fmt::Debug for Height {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Height({})", self.0)
+    }
+}
+
+impl malachitebft_core_types::Height for Height {
+    const ZERO: Self = Height::new(0);
+    const INITIAL: Self = Height::new(1);
+
+    fn increment_by(&self, n: u64) -> Self {
+        Self(self.0 + n)
+    }
+
+    fn decrement_by(&self, n: u64) -> Option<Self> {
+        Some(Self(self.0.saturating_sub(n)))
+    }
+
+    fn as_u64(&self) -> u64 {
+        self.0
+    }
+}
+
+impl Protobuf for Height {
+    type Proto = u64;
+
+    fn from_proto(proto: Self::Proto) -> Result<Self, ProtoError> {
+        Ok(Self(proto))
+    }
+
+    fn to_proto(&self) -> Result<Self::Proto, ProtoError> {
+        Ok(self.0)
+    }
+}
--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -1,15 +1,5 @@
-#![allow(missing_docs)] // crate-level: suppresses missing_docs for the whole crate
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#![forbid(unsafe_code)]
+#![deny(trivial_casts, trivial_numeric_casts)]
+#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod height;
