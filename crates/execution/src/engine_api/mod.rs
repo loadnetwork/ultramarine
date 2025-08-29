165d5deb97d2ccd785a3a878c48d1d42d624adc1
allnil <nil.medvedev17@gmail.com>
2025-08-29 10:23:51 +0100

    feat: exchange_capabilities call

--- crates/execution/src/engine_api/mod.rs
+++ crates/execution/src/engine_api/mod.rs
@@ -6,9 +6,13 @@ pub mod jwt;
 
 use alloy_rpc_types_engine::{
     ExecutionPayloadV3, ForkchoiceState, ForkchoiceUpdated, PayloadAttributes, PayloadId,
+    PayloadStatus,
 };
 use async_trait::async_trait;
 use color_eyre::eyre;
+use ultramarine_types::aliases::{B256, BlockHash};
+
+use crate::engine_api::capabilities::EngineCapabilities;
 
 /// The Engine API trait, defining the interface between Consensus and Execution layers.
 #[async_trait]
@@ -20,4 +24,13 @@ pub trait EngineApi: Send + Sync {
     ) -> eyre::Result<ForkchoiceUpdated>;
 
     async fn get_payload(&self, payload_id: PayloadId) -> eyre::Result<ExecutionPayloadV3>;
+
+    async fn new_payload(
+        &self,
+        execution_payload: ExecutionPayloadV3,
+        versioned_hashes: Vec<B256>,
+        parent_block_hash: BlockHash,
+    ) -> eyre::Result<PayloadStatus>;
+
+    async fn exchange_capabilities(&self) -> eyre::Result<EngineCapabilities>;
 }
