5c41e8f5372ae17437f965fa23394693f59c551d
allnil <nil.medvedev17@gmail.com>
2025-12-19 14:41:30 +0000

    fix: pin protoc versions

--- crates/types/build.rs
+++ crates/types/build.rs
@@ -1,12 +1,19 @@
 #![allow(missing_docs)]
 
 use std::io::Result;
+use std::env;
 
 fn main() -> Result<()> {
     let protos = &["proto/consensus.proto", "proto/sync.proto", "proto/liveness.proto"];
 
     for proto in protos {
         println!("cargo:rerun-if-changed={proto}");
+    }
+
+    if env::var("PROTOC").is_err() {
+        let protoc = protoc_bin_vendored::protoc_bin_path()
+            .map_err(|error| std::io::Error::new(std::io::ErrorKind::Other, error))?;
+        env::set_var("PROTOC", protoc);
     }
 
     let mut config = prost_build::Config::new();
